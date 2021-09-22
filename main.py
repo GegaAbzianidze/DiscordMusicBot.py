2ab0409af2bb360734e85c70597a364dbea92996
Gega <49879577+gegaa@users.noreply.github.com>
2021-09-22 23:49:05 +0400

    new

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import music
 cogs = [music]
 
 client = commands.Bot(command_prefix='!',
-intents = discord.Intents.all())
+intents = discord.Intents.all()) 
 
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
 
-client.run("Your token Here")
+client.run("ODkwMjQxNDQ5MzY5Njg2MDY2.YUs79Q.IVoVxKFZy523B9Jz4rEMIavvyt4")
 
